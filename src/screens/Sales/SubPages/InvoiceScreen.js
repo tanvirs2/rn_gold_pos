5a70f924361a7edadd383e4bdfaa6fd86ed698a4
tanvirs2 <tanvir603@gmail.com>
2022-05-18 16:10:54 +0600

    iconbutton component

---
+++ src/screens/Sales/SubPages/InvoiceScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import {Text, View} from 'react-native';
+
+const Invoice = () => {
+  return (
+      <View>
+          <Text>Invoice</Text>
+      </View>
+  )
+}
+
+export default Invoice;
