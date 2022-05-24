f6d888f880e079a347aeb15cc188a4b9f4b29db8
tanvirs2 <tanvir603@gmail.com>
2022-05-24 20:05:50 +0600

    Menu optimize

---
+++ src/settings/menuObject.js
@@ -0,0 +1,35 @@
+const menuObject = {
+  menuItems: [
+    {
+      mainMenu: 'Sell Management',
+      mainIcon: '',
+      subMenu: [
+        {
+          name: 'Sell',
+          icon: '',
+        },
+        {
+          name: 'Sell Return',
+          icon: '',
+        },
+      ],
+    },
+    {
+      mainMenu: 'Purchase',
+      mainIcon: '',
+      subMenu: [
+        {
+          name: 'Gold Purchase',
+          icon: '',
+        },
+        {
+          name: 'Old Gold Purchase',
+          icon: '',
+        },
+
+      ],
+    },
+  ],
+};
+
+export default menuObject;
